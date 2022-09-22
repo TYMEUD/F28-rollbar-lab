241b24fbead6a0bce11bfded1017a0bfd1edfa6f
Ty Menlove <tjmundecided@gmail.com>
2022-09-22 15:23:22 -0600

    added rollbar warning

--- server.js
+++ server.js
@@ -29,22 +29,22 @@ app.get('/api/movies', (req, res) => {
 })
 
 app.post('/api/movies', (req, res) => {
-   let {name} = req.body
+   let {title} = req.body
 
-   const index = movies.findIndex(student => {
-       return student === name
+   const index = movies.findIndex(movie => {
+       return movie === title
    })
 
    try {
-       if (index === -1 && name !== '') {
-           movies.push(name)
+       if (index === -1 && title !== '') {
+           movies.push(title)
            rollbar.log('Movie was added successfully')
            res.status(200).send(movies)
-       } else if (name === ''){
+       } else if (title === ''){
             rollbar.error('No movie was provided')
            res.status(400).send('You must enter a movie.')
        } else {
-           rollbar.error('Movie is already in array')
+           rollbar.warning('Movie is already in array')
            res.status(400).send('That movie already exists.')
        }
    } catch (err) {
@@ -56,7 +56,7 @@ app.delete('/api/movies/:index', (req, res) => {
     const targetIndex = +req.params.index
     
     students.splice(targetIndex, 1)
-    rollbar.info('Student was deleted')
+    rollbar.info('Movie was deleted')
     res.status(200).send(students)
 })
 
